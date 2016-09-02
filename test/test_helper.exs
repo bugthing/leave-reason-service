787c97b9a9a89cf8304b5028f970848971155cf4
Benjamin Martin <benjamin.martin@workplacesystems.com>
2016-09-02 23:06:26 +0100

    Remove not required Mix tasks in test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,11 +1,3 @@
 ExUnit.start
 
-#Mix.Task.run "ecto.create", ~w(-r LeaveReasonService.Repo --quiet)
-#Mix.Task.run "ecto.migrate", ~w(-r LeaveReasonService.Repo --quiet)
-Mix.Task.run "ecto.create", ["--quiet"]
-Mix.Task.run "ecto.migrate", ["--quiet"]
-#Ecto.Adapters.SQL.begin_test_transaction(LeaveReasonService.Repo)
 Ecto.Adapters.SQL.Sandbox.mode(LeaveReasonService.Repo, :manual)
-
-
-
